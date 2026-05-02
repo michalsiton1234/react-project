fc1b75a41dc63de8e7347f68ff9dad1863511cc7
michalsiton1234 <ys7659356@gmail.com>
2026-05-03 00:57:41 +0200

    change

--- src/api/EmployerApi.ts
+++ src/api/EmployerApi.ts
@@ -35,6 +35,7 @@ export const employerAPI = {
     return response.data;
   },
 
+
   // POST: api/Employer - יצירת מעסיק חדש
   create: async (employerData: Employer) => {
     const response = await api.post<Employer>("/Employer", employerData);
